c26d984c203944f1c57333dfcfb6895c03282b0a
victory1908 <victory1908@gmail.com>
2018-10-07 22:55:51 +0800

    Initial Commit

---
+++ VicTmdb/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  VicTmdb
+//
+//  Created by victory1908 on 7/10/18.
+//  Copyright © 2018 victory1908. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VicTmdb/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VicTmdb
+//
+//  Created by victory1908 on 7/10/18.
+//  Copyright © 2018 victory1908. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
